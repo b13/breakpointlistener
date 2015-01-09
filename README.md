1506706916a3a5dec9464b8576112207d3561ed3
Benjamin Gröner <bgroener@Benjis-MacBook-2.local>
2015-01-09 11:48:14 +0100

    Documentation and cleanup

---
+++ README.md
@@ -0,0 +1,66 @@
+Breakpoint Listener
+===================
+
+Introduction
+------------
+
+The breakpointListener is a small tool the listen to breakpoint changes in JS code in a resource economic way. Its 
+purpose is to register handlers that will be called on breakpoint changes.
+
+### Initialization with AMD
+
+	require(['breakpointlistener'], function(BPL){
+		var BreakpointListener = new BPL({
+			breakpoints: {
+				"xs": 0,
+				"sm": 480,
+				"md": 768,
+				"lg": 1024
+			}
+		});
+	});
+	
+Simply load the breakpoint listener as AMD module and create new instances of it. It is required to define the 
+breakpoints like in the following example, but it is not required to define 4 breakpoints you could define any number of 
+breakpoints that is bigger than 1. You could chose any name you want for the breakpoints, but it is recommended to use
+the commonly used identifiers xs, sm, md and lg.
+
+### Getting the current breakpoint
+	
+	BreakpointListener.getCurrentBreakpoint();
+
+
+### Binding breakpoint change handlers
+
+	BreakpointListener.onChangeBreakpoint(function(evt){
+		switch(evt.currentBreakpoint){
+			case "xs":
+				if(evt.lastBreakpoint === "sm"){
+					. . .
+				}
+				break;
+			case "sm":
+				. . .
+				break;
+			case "md":
+				. . .
+				break;
+			case "lg":
+				. . .
+				break;
+		}
+		
+		. . .
+	});
+
+registerBreakpointHandler(handler); is equivalent to onChangeBreakpoint(handler);
+
+	BreakpointListener.registerBreakpointHandler(function(evt){
+		. . .
+	});
+
+Simply register callbacks to listen to breakpoint changes. The callback will receive an evt parameter, that contains:
+	- currentBreakpoint	// The breakpoint that is now active
+	- lastBreakpoint	// The breakpoint that was active before the breakpoint change
+	- timestamp			// Time when the change occurred
+	
--- src/breakpointlistener.js
+++ src/breakpointlistener.js
@@ -3,70 +3,107 @@ define('breakpointlistener'
 , function($){
 	var BreakpointListener = function(opts){
 		var
-			bph = this
+			bpl = this
 			, defaultOptions = {};
 
+		/**
+		 * Initialize the breakpointListener intance
+		 * @param opts
+		 */
 		function initialize(opts) {
-			bph.options = $.extend(defaultOptions, opts || {});
+			bpl.options = $.extend(defaultOptions, opts || {});
 
-			bph.breakpointHandlers = [];
-			bph.currentBreakPoint = bph.getCurrentBreakPoint();
+			bpl.breakpointHandlers = [];
+			bpl.currentBreakpoint = bpl.getCurrentBreakpoint();
 
 			bindEvents();
 		}
 
+		/**
+		 * Binding events
+		 */
 		function bindEvents() {
 			$(window).on('resize', onResize);
 		}
 
+		/**
+		 * Resize handler
+		 */
 		function onResize() {
 			var evt = {
-				lastBreakPoint     : bph.currentBreakPoint
-				,currentBreakPoint :bph.updateBreakPoint()
+				lastBreakpoint     : bpl.currentBreakpoint
+				,currentBreakpoint :bpl.updateBreakpoint()
 				,timestamp : new Date()
 			};
 
-			if(evt.lastBreakPoint != evt.currentBreakPoint){
-				$.each(bph.breakpointHandlers, function(index, handler){
+			if(evt.lastBreakpoint != evt.currentBreakpoint){
+				$.each(bpl.breakpointHandlers, function(index, handler){
 					handler(evt);
 				});
 			}
 		}
 
-		bph.getCurrentBreakPoint = function(){
-			if(!bph.currentBreakPoint){
-				bph.updateBreakPoint();
+		/**
+		 * Returns a string representation of the current breakpoint
+		 * @returns {*}
+		 */
+		bpl.getCurrentBreakpoint = function(){
+			if(!bpl.currentBreakpoint){
+				bpl.updateBreakpoint();
 			}
-			return bph.currentBreakPoint;
+			return bpl.currentBreakpoint;
 		};
 
-		bph.offChangeBreakpoint = function(handler){
-			bph.unregisterBreakPointHandler(handler);
+		/**
+		 * Unregister a breakpoint change handler.
+		 * Does the same as unregisterBreakpointHandler.
+		 * @param handler
+		 */
+		bpl.offChangeBreakpoint = function(handler){
+			bpl.unregisterBreakpointHandler(handler);
 		};
 
-		bph.onChangeBreakpoint = function(handler){
-			bph.registerBreakPointHandler(handler);
+		/**
+		 * Register a breakpoint change handler.
+		 * Does tha same as registerBreakpointHandler.
+		 * @param handler
+		 */
+		bpl.onChangeBreakpoint = function(handler){
+			bpl.registerBreakpointHandler(handler);
 		};
 
-		bph.registerBreakPointHandler = function(handler){
+		/**
+		 * Register a breakpoint change handler.
+		 * @param handler
+		 */
+		bpl.registerBreakpointHandler = function(handler){
 			if(_.isFunction(handler)){
-				bph.breakpointHandlers.push(handler);
+				bpl.breakpointHandlers.push(handler);
 			}
 		};
 
-		bph.unregisterBreakPointHandler = function(handler){
-			bph.breakpointHandlers = _.without(bph.breakpointHandlers, handler);
+		/**
+		 * Unregister a breakpoint change handler.
+		 * @param handler
+		 */
+		bpl.unregisterBreakpointHandler = function(handler){
+			bpl.breakpointHandlers = _.without(bpl.breakpointHandlers, handler);
 		};
 
-		bph.updateBreakPoint = function(){
+		/**
+		 * Function that computes which breakpoint is the current one.
+		 * Can be triggered manually if needed.
+		 * @returns {string}
+		 */
+		bpl.updateBreakpoint = function(){
 			var width = $(window).width()
 				, breakpoint = 'xs';
 
-			$.each(bph.options.breakpoints, function(key, val){
+			$.each(bpl.options.breakpoints, function(key, val){
 				if(width < val) return breakpoint;
 				breakpoint = key;
 			});
-			bph.currentBreakPoint = breakpoint;
+			bpl.currentBreakpoint = breakpoint;
 
 			return breakpoint;
 		};
